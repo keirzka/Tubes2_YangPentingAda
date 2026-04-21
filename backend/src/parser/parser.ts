1c47621cdf04842055b42892adfff8e08cebac4d
pidipam <vara.azzara@gmail.com>
2026-04-21 16:42:19 +0700

    feat: skeleton parser

---
+++ backend/src/parser/parser.ts
@@ -0,0 +1,6 @@
+import type { DOMTree } from "../types/dom.js";
+import { ParserError } from "../types/constants.js";
+
+export function parseHTML(html: string): DOMTree {
+  throw new Error("Not implemented yet");
+}
